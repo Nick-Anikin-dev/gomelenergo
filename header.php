db8c038fed27a2084db618ee8a387bb7f3cda645
Nick Anikin <nick.anikin.2000@gmail.com>
2021-07-07 12:30:08 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
+    <title>Социальная сфера</title>
+</head>
+<body>
+<header>
+    <div class="beforeNav">
+        <div class="phoneWrapper"><a class="link" href="tel:522-76-11">
+                <svg id="phoneIcon" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7964 9.84578C12.878 9.84578 11.9763 9.70215 11.1218 9.41977C10.7031 9.27695 10.1884 9.40797 9.93285 9.67043L8.24625 10.9436C6.29027 9.89953 5.08543 8.69508 4.05559 6.75379L5.29133 5.11113C5.61238 4.79051 5.72754 4.32215 5.58957 3.8827C5.30598 3.02371 5.16191 2.12242 5.16191 1.20367C5.16195 0.539961 4.62199 0 3.95832 0H1.20363C0.539961 0 0 0.539961 0 1.20363C0 8.81105 6.18898 15 13.7964 15C14.4601 15 15 14.46 15 13.7964V11.0494C15 10.3857 14.46 9.84578 13.7964 9.84578Z" fill="white"/>
+                </svg>
+                +7 (944) 522-76-11</a></div>
+        <div class="searchInputWrapper"><input type="text" placeholder="Поиск..."></div>
+        <div class="viewVariantsWrapper">
+            <img src="../assets/images/eyeIcon.png" id="eyeIcon" alt="Version for the visually impaired">
+            <span id="changeLanguage">En</span>
+        </div>
+    </div>
+    <nav class="navMenu">
+        <li class="logoWrapper">
+            <div class="logo"></div>
+            <div>
+                Гомельское республиканское унитарное предприятие электроэнергетики “Гомельэнерго”
+            </div>
+        </li>
+        <li>О предприятии</li>
+        <li>Физическим лицам</li>
+        <li>Юридическим лицам</li>
+        <li>Пресс-центр</li>
+        <li>Контакты</li>
+    </nav>
+</header>
+
---
+++ social-sphere.php
@@ -0,0 +1,50 @@
+<?php get_header() ?>
+<main class="socialSphere">
+    <section class="preview">
+        <div class="contentWrapper">
+            <nav class="previewNav">
+                <li><a href="#">Вернуться на главную <img src="../assets/images/arrow.svg" alt="link to MainPage"></a>
+                </li>
+                <li><a href="#">К контактам</a></li>
+                <li><a href="#">Вниз</a></li>
+            </nav>
+            <div class="description">
+                <h2>Социальная сфера</h2>
+                <p>
+                    На данной странице можно найти
+                    актуальную информацию о социальной сфере РУП «Гомельэнерго» <br>
+                    а также информацию о нашем санатории
+                </p>
+                <div class="line"></div>
+            </div>
+            <div class="showMore">
+                <a href="">Увидеть больше</a>
+            </div>
+        </div>
+    </section>
+    <section class="sanatorium">
+        <div>
+            <h3>Санаторий Гомельэнерго</h3>
+            <p>
+                Lorem ipsum dolor si amet, consectetur adipiscing elitsed do
+                eiusmodtempo incididunt ut labore et dolore magna aliqua.
+                Utenim ad minim veniam, nostrud exercitation ullamco laboris
+                aliquipexeaco mmodo consequat.
+                Duis aute irure dolor in reprehen voluptate velit esse cillum
+                dolore eu fugiat nulla incididunt ut labore dolore pariatur.
+            </p>
+            <div class="learnMoreBtn">Узнать больше</div>
+        </div>
+        <div class="sliderHandler">
+            <div class="topArrow"></div>
+            <div class="dottedWrapper">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="buttonArrow"></div>
+        </div>
+        <div class="imgWrapper"></div>
+    </section>
+</main>
+<?php get_footer(); ?>
